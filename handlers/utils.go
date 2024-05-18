fd516e5532168c069de7109a9d817bb7b672ca88
Nordine Bittich <nordine@bittich.be>
2024-05-18 15:34:01 +0200

    check ok

--- handlers/utils.go
+++ handlers/utils.go
@@ -12,8 +12,11 @@ import (
 func renderHTML(statusCode int, c echo.Context, tpl templ.Component) error {
 	c.Response().Status = statusCode
 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
-	csrf := c.Get("csrf").(string)
-	ctx := context.WithValue(c.Request().Context(), types.CsrfKey, csrf)
+	ctx := c.Request().Context()
+
+	if csrf, ok := c.Get("csrf").(string); ok {
+		ctx = context.WithValue(c.Request().Context(), types.CsrfKey, csrf)
+	}
 	msg := types.Message{}
 	if err := c.Bind(&msg); err == nil && msg.Message != "" {
 		ctx = context.WithValue(ctx, types.MessageKey, msg)
