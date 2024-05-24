44e7968b56f36fdca1a580f7c2a74dbe4dab89b6
Nordine Bittich <nordine@bittich.be>
2024-05-24 14:34:34 +0200

    omitempty

--- jobs/openfoodfacts/cmd/txtToStruct.go
+++ jobs/openfoodfacts/cmd/txtToStruct.go
@@ -13,6 +13,7 @@ func main() {
 	s := "type OpenFoodFactCSVEntry struct {\n"
 	for _, field := range strings.Split(fields, "\n") {
 		field = strings.ReplaceAll(strings.TrimSpace(field), "-", "")
+		field = strings.ReplaceAll(field, "url", "URL")
 		if len(field) == 0 {
 			continue
 		}
@@ -20,11 +21,12 @@ func main() {
 		for i := range temp {
 			temp[i] = strings.Title(temp[i])
 		}
-		s += fmt.Sprintf("%s string `json:\"%s\" bson:\"%s\"`\n", strings.Join(temp, ""), field, field)
+		if field == "code" {
+			s += fmt.Sprintf("%s string `json:\"_id\" bson:\"_id\"`\n", strings.Join(temp, ""))
+		} else {
+			s += fmt.Sprintf("%s string `json:\"%s,omitempty\" bson:\"%s,omitempty\"`\n", strings.Join(temp, ""), field, field)
+		}
 	}
-
-	s += "//keep stuff that are not in the csv at the end\n"
-	s += "ID string `bson:\"_id\" json:\"_id\"`\n"
 	s += "}\n"
 	fmt.Println(s)
 }
