a3992c85e99a9e6e7d573d11d77ad50bf4a8b803
Nordine Bittich <nordine@bittich.be>
2024-05-25 21:05:08 +0200

    improvements

--- jobs/openfoodfacts/cmd/txtToStruct.go
+++ jobs/openfoodfacts/cmd/txtToStruct.go
@@ -4,31 +4,50 @@ import (
 	_ "embed"
 	"fmt"
 	"strings"
+	"unicode"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 //go:embed data-fields.txt
 var fields string
 
+func firstToLower(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	r := []rune(s)
+	r[0] = unicode.ToLower(r[0])
+
+	return string(r)
+}
+
 func main() {
 	s := "type OpenFoodFactCSVEntry struct {\n"
 	for _, field := range strings.Split(fields, "\n") {
-		field = strings.ReplaceAll(strings.TrimSpace(field), "-", "")
+		field = strings.ReplaceAll(strings.TrimSpace(field), "-", "_")
 		field = strings.ReplaceAll(field, "url", "URL")
 		if len(field) == 0 {
 			continue
 		}
 		temp := strings.Split(field, "_")
 		for i := range temp {
-			temp[i] = strings.Title(temp[i])
+			temp[i] = cases.Title(language.Und).String(temp[i])
 		}
 		if field == "code" {
 			s += fmt.Sprintf("%s string `json:\"_id\" bson:\"_id\"`\n", strings.Join(temp, ""))
 		} else {
-			f := fmt.Sprintf("\"%s,omitempty\"", field)
+			goFieldName := strings.Join(temp, "")
+			jsonFieldName := firstToLower(goFieldName)
+			f := fmt.Sprintf("\"%s,omitempty\"", jsonFieldName)
 			t := "string"
+
 			if strings.HasSuffix(field, "_t") ||
 				strings.HasSuffix(field, "_100g") ||
 				strings.HasSuffix(field, "_serving") ||
+				strings.HasSuffix(field, "_quantity") ||
 				strings.HasSuffix(field, "_n") {
 				t = "int"
 			}
@@ -36,10 +55,20 @@ func main() {
 			if strings.HasSuffix(field, "_datetime") {
 				t = "types.TimeISO8601"
 			}
-			if field == "creator" {
+
+			if field == "completeness" {
+				t = "float64"
+			}
+			if field == "creator" ||
+				field == "states" ||
+				field == "no_nutrition_data" ||
+				field == "last_modified_by" ||
+				strings.HasPrefix(field, "packaging") ||
+				strings.HasPrefix(field, "popularity") {
 				f = "\"-\""
 			}
-			s += fmt.Sprintf("%s %s `json:%s bson:%s`\n", strings.Join(temp, ""), t, f, f)
+
+			s += fmt.Sprintf("%s %s `json:%s bson:%s`\n", goFieldName, t, f, f)
 		}
 	}
 	s += "}\n"
